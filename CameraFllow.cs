8de9ed756091d4d49e181f2d6cec5b772b543c2f
palxu <37163063+palxu@users.noreply.github.com>
2018-05-11 17:50:11 +0800

    Add files via upload

---
+++ CameraFllow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFllow : MonoBehaviour
+{
+
+    public Transform target;
+    public float distanceH = 7f;
+    public float distanceV = 4f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        Vector3 nextpos = target.forward * -1 * distanceH + target.up * distanceV + target.position;
+
+        this.transform.position = nextpos;
+
+        this.transform.LookAt(target);
+    }
+}
